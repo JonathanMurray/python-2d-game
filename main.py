8d937d42ff34aba95c97f591db82e901c8cc09bd
jmurray <jonte.murray@gmail.com>
2018-08-20 20:05:58 +0200

    Add camera that follows player movement

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ class Direction(Enum):
 	UP = 3
 	DOWN = 4
 
-def render_box(screen, box):
-	pygame.draw.rect(screen, box.color, box.rect())
+def render_box(screen, box, camera_pos):
+	pygame.draw.rect(screen, box.color, (box.x - camera_pos[0], box.y - camera_pos[1], box.w, box.h))
 
 def ranges_overlap(a_min, a_max, b_min, b_max):
     return (a_min <= b_max) and (b_min <= a_max)
@@ -48,14 +48,17 @@ def rects_intersect(r1, r2):
 
 BG_COLOR = (200,200,200)
 SCREEN_SIZE = (500,500)
+CAMERA_SIZE = (500, 470)
+GAME_WORLD_SIZE = (600,600)
 
 pygame.init()
 pygame.font.init()
 font = pygame.font.SysFont('Arial', 30)
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
+camera_pos = (0, 0)
 player = PlayerBox(Box((100, 100), (50, 50), (250,250,250)), Direction.RIGHT, 0, 9)
-food_boxes = [Box((150, 350), (30, 30), (50, 200, 50)), Box((250, 300), (30, 30), (50, 200, 50))]
+food_boxes = [Box((150, 350), (30, 30), (50, 200, 50)), Box((450, 300), (30, 30), (50, 200, 50))]
 enemy_boxes = [Box((320, 220), (50, 50), (250, 0, 0))]
 health = 3
 
@@ -79,11 +82,14 @@ while(True):
 	if player.direction == Direction.LEFT:
 		player.box.x = max(player.box.x - player.speed, 0)
 	elif player.direction == Direction.RIGHT:
-		player.box.x = min(player.box.x + player.speed, SCREEN_SIZE[0] - player.box.w)
+		player.box.x = min(player.box.x + player.speed, GAME_WORLD_SIZE[0] - player.box.w)
 	elif player.direction == Direction.UP:
 		player.box.y = max(player.box.y - player.speed, 0)
 	elif player.direction == Direction.DOWN:
-		player.box.y = min(player.box.y + player.speed, SCREEN_SIZE[1] - player.box.h)
+		player.box.y = min(player.box.y + player.speed, GAME_WORLD_SIZE[1] - player.box.h)
+
+	camera_pos = (min(max(player.box.x - CAMERA_SIZE[0] / 2, 0), GAME_WORLD_SIZE[0] - CAMERA_SIZE[0]), min(max(player.box.y - CAMERA_SIZE[1] / 2, 0), GAME_WORLD_SIZE[1] - CAMERA_SIZE[1]))
+
 
 	food_boxes_to_delete = []
 	for box in food_boxes:
@@ -100,10 +106,11 @@ while(True):
 	enemy_boxes = [b for b in enemy_boxes if b not in enemy_boxes_to_delete]
 
 	screen.fill(BG_COLOR)
-	render_box(screen, player.box)
+	render_box(screen, player.box, camera_pos)
 	for box in food_boxes + enemy_boxes:
-		render_box(screen, box)
-	text_surface = font.render(str(health) + " HP", False, (0, 0, 0))
-	screen.blit(text_surface, (100, 480))
+		render_box(screen, box, camera_pos)
+	text_surface = font.render("[" + str(health) + " HP]    [position (" + str(player.box.x) + ", " + str(player.box.y) + ")]", False, (0, 0, 0))
+	screen.blit(text_surface, (100, 475))
+	pygame.draw.rect(screen, (0, 0, 100), (0, 0, CAMERA_SIZE[0], CAMERA_SIZE[1]), 3)
 	pygame.display.update()
 
