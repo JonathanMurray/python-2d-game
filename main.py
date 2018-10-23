353e577f7335b8fb9c48fffcbc02d43b2d5debb0
jmurray <jonte.murray@gmail.com>
2018-10-23 23:48:10 +0200

    Extract code for getting user input/actions to new file user_input.py

--- main.py
+++ main.py
@@ -3,21 +3,14 @@
 import pygame
 import sys
 
-from common import Direction, boxes_intersect
+from common import boxes_intersect
 from game_world_init import init_game_state_from_file
+from user_input import *
 from view import View, ScreenArea
 
 GAME_WORLD_SIZE = (1000, 1000)
 SCREEN_SIZE = (700, 600)
 CAMERA_SIZE = (700, 500)
-
-PYGAME_MOVEMENT_KEYS = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
-DIRECTION_BY_PYGAME_MOVEMENT_KEY = {
-    pygame.K_LEFT: Direction.LEFT,
-    pygame.K_RIGHT: Direction.RIGHT,
-    pygame.K_UP: Direction.UP,
-    pygame.K_DOWN: Direction.DOWN
-}
 
 game_state = init_game_state_from_file(GAME_WORLD_SIZE, CAMERA_SIZE)
 pygame.init()
@@ -30,44 +23,26 @@ clock = pygame.time.Clock()
 ticks_since_ai_ran = 0
 AI_RUN_INTERVAL = 750
 
-movement_keys_down = []
-
 while True:
 
     # ------------------------------------
     #         HANDLE EVENTS
     # ------------------------------------
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    user_actions = get_user_actions()
+    for action in user_actions:
+        if isinstance(action, ActionExitGame):
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key in PYGAME_MOVEMENT_KEYS:
-                if event.key in movement_keys_down:
-                    movement_keys_down.remove(event.key)
-                movement_keys_down.append(event.key)
-            elif event.key == pygame.K_a:
-                game_state.try_use_heal_ability()
-            elif event.key == pygame.K_f:
-                game_state.try_use_attack_ability()
-            elif event.key == pygame.K_1:
-                game_state.try_use_health_potion(1)
-            elif event.key == pygame.K_2:
-                game_state.try_use_health_potion(2)
-            elif event.key == pygame.K_3:
-                game_state.try_use_health_potion(3)
-            elif event.key == pygame.K_4:
-                game_state.try_use_health_potion(4)
-            elif event.key == pygame.K_5:
-                game_state.try_use_health_potion(5)
-        if event.type == pygame.KEYUP:
-            if event.key in PYGAME_MOVEMENT_KEYS:
-                movement_keys_down.remove(event.key)
-        if movement_keys_down:
-            last_pressed_movement_key = movement_keys_down[-1]
-            game_state.player_entity.set_moving_in_dir(DIRECTION_BY_PYGAME_MOVEMENT_KEY[last_pressed_movement_key])
-        else:
+        elif isinstance(action, ActionTryUseHealAbility):
+            game_state.try_use_heal_ability()
+        elif isinstance(action, ActionTryUseAttackAbility):
+            game_state.try_use_attack_ability()
+        elif isinstance(action, ActionTryUseHealthPotion):
+            game_state.try_use_health_potion(action.slot_number)
+        elif isinstance(action, ActionMoveInDirection):
+            game_state.player_entity.set_moving_in_dir(action.direction)
+        elif isinstance(action, ActionStopMoving):
             game_state.player_entity.set_not_moving()
 
     # ------------------------------------
---
+++ user_input.py
@@ -0,0 +1,76 @@
+import pygame
+from common import Direction
+
+
+class ActionExitGame:
+    pass
+
+
+class ActionTryUseHealAbility:
+    pass
+
+
+class ActionTryUseAttackAbility:
+    pass
+
+
+class ActionTryUseHealthPotion:
+    def __init__(self, slot_number):
+        self.slot_number = slot_number
+
+
+class ActionMoveInDirection:
+    def __init__(self, direction):
+        self.direction = direction
+
+
+class ActionStopMoving:
+    pass
+
+
+PYGAME_MOVEMENT_KEYS = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
+DIRECTION_BY_PYGAME_MOVEMENT_KEY = {
+    pygame.K_LEFT: Direction.LEFT,
+    pygame.K_RIGHT: Direction.RIGHT,
+    pygame.K_UP: Direction.UP,
+    pygame.K_DOWN: Direction.DOWN
+}
+movement_keys_down = []
+
+
+def get_user_actions():
+    actions = []
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            actions.append(ActionExitGame())
+        if event.type == pygame.KEYDOWN:
+            if event.key in PYGAME_MOVEMENT_KEYS:
+                if event.key in movement_keys_down:
+                    movement_keys_down.remove(event.key)
+                movement_keys_down.append(event.key)
+            elif event.key == pygame.K_a:
+                actions.append(ActionTryUseHealAbility())
+            elif event.key == pygame.K_f:
+                actions.append(ActionTryUseAttackAbility())
+            elif event.key == pygame.K_1:
+                actions.append(ActionTryUseHealthPotion(1))
+            elif event.key == pygame.K_2:
+                actions.append(ActionTryUseHealthPotion(2))
+            elif event.key == pygame.K_3:
+                actions.append(ActionTryUseHealthPotion(3))
+            elif event.key == pygame.K_4:
+                actions.append(ActionTryUseHealthPotion(4))
+            elif event.key == pygame.K_5:
+                actions.append(ActionTryUseHealthPotion(5))
+        if event.type == pygame.KEYUP:
+            if event.key in PYGAME_MOVEMENT_KEYS:
+                movement_keys_down.remove(event.key)
+
+        if movement_keys_down:
+            last_pressed_movement_key = movement_keys_down[-1]
+            direction = DIRECTION_BY_PYGAME_MOVEMENT_KEY[last_pressed_movement_key]
+            actions.append(ActionMoveInDirection(direction))
+        else:
+            actions.append(ActionStopMoving())
+
+    return actions
