JonathanMurray/python-2d-game

commit 50adcd508b674dc00393e3cec08f4d843f297e2c

Add more typing information

pythongame/game_data.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from pythongame.common import *
 
 PLAYER_ENTITY_SIZE = (60, 60)
@@ -38,18 +40,18 @@ class AbilityData:
         self.cooldown = cooldown
 
 
-ENTITY_SPRITE_INITIALIZERS = {
+ENTITY_SPRITE_INITIALIZERS: Dict[Sprite, SpriteInitializer] = {
     Sprite.PLAYER: SpriteInitializer("resources/player.png", PLAYER_ENTITY_SIZE),
     Sprite.WALL: SpriteInitializer("resources/stone_tile.png", WALL_SIZE)
 }
 
-UI_ICON_SPRITE_PATHS = {}
+UI_ICON_SPRITE_PATHS: Dict[UiIconSprite, str] = {}
 
-POTION_ICON_SPRITES = {}
+POTION_ICON_SPRITES: Dict[PotionType, UiIconSprite] = {}
 
-ABILITIES = {}
+ABILITIES: Dict[AbilityType, AbilityData] = {}
 
-BUFF_TEXTS = {}
+BUFF_TEXTS: Dict[BuffType: str] = {}
 
 
 def register_ability_data(ability_type: AbilityType, ability_data: AbilityData):

pythongame/abilities.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from pythongame.common import *
 from pythongame.game_state import GameState
 
@@ -6,8 +8,11 @@ def apply_ability_effect(game_state: GameState, ability_type: AbilityType):
     ability_effects[ability_type](game_state)
 
 
-ability_effects = {}
+ability_effects: Dict[AbilityType, Any] = {}
 
 
-def register_ability_effect(ability_type, effect_function):
+# TODO Create an interface for the ability effect functions
+
+
+def register_ability_effect(ability_type: AbilityType, effect_function):
     ability_effects[ability_type] = effect_function

pythongame/buffs.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from pythongame.common import *
 from pythongame.game_state import GameState
 
@@ -13,7 +15,7 @@ class AbstractBuff:
         pass
 
 
-BUFF_EFFECTS = {}
+BUFF_EFFECTS: Dict[BuffType, AbstractBuff] = {}
 
 
 def register_buff_effect(buff_type: BuffType, effect: AbstractBuff):

pythongame/enemy_behavior.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from pythongame.common import *
 
 
@@ -5,7 +7,10 @@ def create_enemy_mind(enemy_behavior: EnemyBehavior):
     return enemy_mind_constructors[enemy_behavior]()
 
 
-enemy_mind_constructors = {}
+enemy_mind_constructors: Dict[EnemyBehavior, Any] = {}
+
+
+# TODO: Create an interface for the EnemyMind classes
 
 
 def register_enemy_behavior(enemy_behavior: EnemyBehavior, mind_constructor):

pythongame/potions.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from pythongame.common import *
 from pythongame.game_state import GameState
 from pythongame.visual_effects import VisualCircle
@@ -22,8 +24,11 @@ def try_consume_potion(potion_type: PotionType, game_state: GameState):
     return potion_effects[potion_type](game_state)
 
 
-potion_effects = {}
+potion_effects: Dict[PotionType, Any] = {}
 
 
-def register_potion_effect(potion_type: PotionType, effect_method):
-    potion_effects[potion_type] = effect_method
+# TODO Create an interface for the potion effect functions
+
+
+def register_potion_effect(potion_type: PotionType, effect_function):
+    potion_effects[potion_type] = effect_function

pythongame/projectiles.py
@@ -1,3 +1,5 @@
+from typing import Dict, Type
+
 from pythongame.common import *
 from pythongame.game_state import Projectile, Enemy, GameState
 
@@ -23,8 +25,8 @@ class AbstractProjectileController:
         return False
 
 
-projectile_controllers = {}
+projectile_controllers: Dict[ProjectileType, Type[AbstractProjectileController]] = {}
 
 
-def register_projectile_controller(projectile_type: ProjectileType, controller):
+def register_projectile_controller(projectile_type: ProjectileType, controller: Type[AbstractProjectileController]):
     projectile_controllers[projectile_type] = controller

commit 5d16704b6ca159928819bcb9651c601d4ad464e8

[BUG] Fix broken type annotation (introduced in 50adcd508b674dc00393e3cec08f4d843f297e2c)

pythongame/game_data.py
@@ -53,7 +53,7 @@ POTION_ICON_SPRITES: Dict[PotionType, UiIconSprite] = {}
 
 ABILITIES: Dict[AbilityType, AbilityData] = {}
 
-BUFF_TEXTS: Dict[BuffType: str] = {}
+BUFF_TEXTS: Dict[BuffType, str] = {}
 
 
 def register_ability_data(ability_type: AbilityType, ability_data: AbilityData):