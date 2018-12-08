628996653d1fcbd20ee7e7f66270ef6da000afa5
jmurray <jonte.murray@gmail.com>
2018-12-08 22:28:47 +0100

    Enlarge game window

--- pythongame/main.py
+++ pythongame/main.py
@@ -13,8 +13,8 @@ from pythongame.core.view_state import ViewState
 from pythongame.game_world_init import create_game_state_from_file
 from pythongame.register_game_data import register_all_game_data
 
-SCREEN_SIZE = (700, 600)
-CAMERA_SIZE = (700, 400)
+SCREEN_SIZE = (700, 700)
+CAMERA_SIZE = (700, 500)
 
 register_all_game_data()
 
