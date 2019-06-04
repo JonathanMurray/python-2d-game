7951160c4207007fba349b49db315ffc7e181a7b
jmurray <jonte.murray@gmail.com>
2019-06-04 22:35:38 +0200

    Play sound when player takes dmg (one chosen randomly from many)

--- pythongame/core/sound_player.py
+++ pythongame/core/sound_player.py
@@ -1,15 +1,18 @@
+import random
+from typing import Any, List, Dict
+
 import pygame
 
 from pythongame.core.common import SoundId
 
-_sounds = {}
+_sounds_by_id: Dict[SoundId, List[Any]] = {}
 
 
 def init_sound_player():
-    global _sounds
-    if _sounds:
+    global _sounds_by_id
+    if _sounds_by_id:
         raise Exception("Don't initialize sound player several times!")
-    _sounds = {
+    _sounds_by_id = {
         SoundId.ABILITY_FIREBALL: load_sound_file('Shot01.ogg'),
         SoundId.ABILITY_WHIRLWIND: load_sound_file('Fire03.ogg'),
         SoundId.ABILITY_TELEPORT: load_sound_file('SciFi06.ogg'),
@@ -20,20 +23,23 @@ def init_sound_player():
         SoundId.EVENT_PLAYER_DIED: load_sound_file('Death01.ogg'),
         SoundId.EVENT_ENEMY_DIED: load_sound_file('Damage02.ogg'),
         SoundId.WARNING: load_sound_file('UI06.ogg'),
-        SoundId.PLAYER_PAIN: load_sound_file('pain1.ogg')
+        SoundId.PLAYER_PAIN: load_sound_file('pain1.ogg', 'pain2.ogg', 'pain3.ogg', 'pain4.ogg')
     }
 
 
 def play_sound(sound_id: SoundId):
-    if not _sounds:
+    if not _sounds_by_id:
         raise Exception("Initialize sound player before playing sounds!")
-    if sound_id in _sounds:
-        _sounds[sound_id].play()
+    if sound_id in _sounds_by_id:
+        sounds = _sounds_by_id[sound_id]
+        randomly_chosen_sound = random.choice(sounds)
+        randomly_chosen_sound.play()
     else:
         raise Exception("No sound defined for: " + str(sound_id))
 
 
-def load_sound_file(filename: str):
-    sound = pygame.mixer.Sound('./resources/sound/' + filename)
-    sound.set_volume(0.1)
-    return sound
+def load_sound_file(*filenames):
+    sounds = [pygame.mixer.Sound('./resources/sound/' + filename) for filename in filenames]
+    for sound in sounds:
+        sound.set_volume(0.1)
+    return sounds
