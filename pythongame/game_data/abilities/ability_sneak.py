151b13a8f9f577ba21eb175ef30a184949713b36
jmurray <jonte.murray@gmail.com>
2019-07-15 16:34:14 +0200

    Add armor after rogue hero exits stealth

--- pythongame/game_data/abilities/ability_sneak.py
+++ pythongame/game_data/abilities/ability_sneak.py
@@ -8,21 +8,20 @@ from pythongame.core.game_data import register_ability_data, AbilityData, regist
 from pythongame.core.game_state import GameState, WorldEntity, NonPlayerCharacter, Event, BuffEventOutcome, \
     PlayerUsedAbilityEvent, PlayerLostHealthEvent
 
-SNEAKING_DURATION = Millis(10000)
-DAMAGE_BONUS_DURATION = Millis(1500)
-SNEAKING_SPEED_DECREASE = 0.5
-SNEAKING_BUFF = BuffType.SNEAKING
-AFTER_SNEAKING_BUFF = BuffType.AFTER_SNEAKING
-SNEAKING_ABILITY_TYPE = AbilityType.SNEAK
+ABILITY_TYPE = AbilityType.SNEAK
+BUFF_SNEAK = BuffType.SNEAKING
+BUFF_POST_SNEAK = BuffType.AFTER_SNEAKING
+DURATION_SNEAK = Millis(10000)
+DURATION_POST_SNEAK = Millis(2500)
+SPEED_DECREASE = 0.5
 DAMAGE_BONUS = 1
+ARMOR_BONUS = 5
 
-
-# TODO Make all enemies consider invis correctly
 
 # TODO Should there be a charge-up time for using invis, to stop it from being used mid-combat as a pure damage bonus?
 
 def _apply_ability(game_state: GameState) -> bool:
-    game_state.player_state.gain_buff_effect(get_buff_effect(SNEAKING_BUFF), SNEAKING_DURATION)
+    game_state.player_state.gain_buff_effect(get_buff_effect(BUFF_SNEAK), DURATION_SNEAK)
     return True
 
 
@@ -30,44 +29,45 @@ class Sneaking(AbstractBuffEffect):
     def apply_start_effect(self, game_state: GameState, buffed_entity: WorldEntity, buffed_npc: NonPlayerCharacter):
         game_state.player_state.is_invisible = True
         # TODO Wearing items that grant movement speed are too effective at countering this
-        game_state.player_entity.add_to_speed_multiplier(-SNEAKING_SPEED_DECREASE)
+        game_state.player_entity.add_to_speed_multiplier(-SPEED_DECREASE)
         game_state.player_state.damage_modifier_bonus += DAMAGE_BONUS
+        game_state.player_state.armor += ARMOR_BONUS
 
     def apply_end_effect(self, game_state: GameState, buffed_entity: WorldEntity, buffed_npc: NonPlayerCharacter):
         game_state.player_state.is_invisible = False
-        game_state.player_entity.add_to_speed_multiplier(SNEAKING_SPEED_DECREASE)
-        game_state.player_state.gain_buff_effect(get_buff_effect(AFTER_SNEAKING_BUFF), DAMAGE_BONUS_DURATION)
+        game_state.player_entity.add_to_speed_multiplier(SPEED_DECREASE)
+        game_state.player_state.gain_buff_effect(get_buff_effect(BUFF_POST_SNEAK), DURATION_POST_SNEAK)
 
     def get_buff_type(self):
-        return SNEAKING_BUFF
+        return BUFF_SNEAK
 
     def buff_handle_event(self, event: Event) -> Optional[BuffEventOutcome]:
-        used_other_ability = isinstance(event, PlayerUsedAbilityEvent) and event.ability != SNEAKING_ABILITY_TYPE
+        used_other_ability = isinstance(event, PlayerUsedAbilityEvent) and event.ability != ABILITY_TYPE
         player_lost_health = isinstance(event, PlayerLostHealthEvent)
         if used_other_ability or player_lost_health:
             return BuffEventOutcome.cancel_effect()
 
 
-# TODO add defensive element to this effect (chance to dodge attacks for example)
 class AfterSneaking(AbstractBuffEffect):
 
     def apply_end_effect(self, game_state: GameState, buffed_entity: WorldEntity, buffed_npc: NonPlayerCharacter):
         game_state.player_state.damage_modifier_bonus -= DAMAGE_BONUS
+        game_state.player_state.armor -= ARMOR_BONUS
 
     def get_buff_type(self):
-        return AFTER_SNEAKING_BUFF
+        return BUFF_POST_SNEAK
 
 
 def register_sneak_ability():
     ui_icon_sprite = UiIconSprite.ABILITY_SNEAK
 
-    register_ability_effect(SNEAKING_ABILITY_TYPE, _apply_ability)
-    description = "Sneaks up on enemies (+" + str(DAMAGE_BONUS * 100) + "% dmg for " \
-                  + "{:.1f}".format(DAMAGE_BONUS_DURATION / 1000) + "s)"
+    register_ability_effect(ABILITY_TYPE, _apply_ability)
+    description = "Invis -> [+" + str(DAMAGE_BONUS * 100) + "% dmg, +" + str(ARMOR_BONUS) + " armor for " \
+                  + "{:.1f}".format(DURATION_POST_SNEAK / 1000) + "s]"
     ability_data = AbilityData("Sneak", ui_icon_sprite, 10, Millis(10000), description, None)
-    register_ability_data(SNEAKING_ABILITY_TYPE, ability_data)
+    register_ability_data(ABILITY_TYPE, ability_data)
     register_ui_icon_sprite_path(ui_icon_sprite, "resources/graphics/sneak_icon.png")
-    register_buff_effect(SNEAKING_BUFF, Sneaking)
-    register_buff_text(SNEAKING_BUFF, "Sneaking")
-    register_buff_effect(AFTER_SNEAKING_BUFF, AfterSneaking)
-    register_buff_text(AFTER_SNEAKING_BUFF, "Element of surprise")
+    register_buff_effect(BUFF_SNEAK, Sneaking)
+    register_buff_text(BUFF_SNEAK, "Sneaking")
+    register_buff_effect(BUFF_POST_SNEAK, AfterSneaking)
+    register_buff_text(BUFF_POST_SNEAK, "Element of surprise")
