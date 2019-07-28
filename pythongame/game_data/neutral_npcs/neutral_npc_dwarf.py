90cd1b09c4c6eefb1f9c2b15d1d3ad1622bedbe5
jmurray <jonte.murray@gmail.com>
2019-07-29 00:32:58 +0200

    (balance) Reduce price of gold and saphire

--- pythongame/game_data/neutral_npcs/neutral_npc_dwarf.py
+++ pythongame/game_data/neutral_npcs/neutral_npc_dwarf.py
@@ -57,9 +57,9 @@ def register_dwarf_npc():
     register_npc_behavior(npc_type, NpcMind)
     introdution = "Hello there. I'm always looking for treasure. If you find any, we might be able to strike a deal!"
     dialog_options = [
-        DialogOptionData("Gold nugget [25 gold]", "sell", BuyTreasure(ItemType.GOLD_NUGGET, 25, "gold nugget"),
+        DialogOptionData("Gold nugget [20 gold]", "sell", BuyTreasure(ItemType.GOLD_NUGGET, 20, "gold nugget"),
                          UiIconSprite.ITEM_GOLD_NUGGET),
-        DialogOptionData("Saphire [40 gold]", "sell", BuyTreasure(ItemType.SAPHIRE, 40, "saphire"),
+        DialogOptionData("Saphire [30 gold]", "sell", BuyTreasure(ItemType.SAPHIRE, 30, "saphire"),
                          UiIconSprite.ITEM_SAPHIRE),
         DialogOptionData("\"Good bye\"", "cancel", None, UiIconSprite.MAP_EDITOR_TRASHCAN)]
     dialog_data = DialogData(PortraitIconSprite.VIKING, introdution, dialog_options)
