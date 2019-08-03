f8580bd6822ed337cea7527c679aa22a51e2e631
jmurray <jonte.murray@gmail.com>
2019-08-03 14:43:24 +0200

    Add script to print data about all items in the game

---
+++ print_items.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from pythongame.core.game_data import ITEMS
+from pythongame.core.item_inventory import ItemEquipmentCategory
+from pythongame.register_game_data import register_all_game_data
+
+
+def items_with_category(category):
+    return [(item_type, ITEMS[item_type]) for item_type in ITEMS
+            if ITEMS[item_type].item_equipment_category == category]
+
+
+def print_items():
+    for category in ItemEquipmentCategory:
+        print(category.name + ":")
+        print("- - - - -")
+        for item_type, item_data in items_with_category(category):
+            print("{:<25}".format(item_type.name) + str(item_data.description))
+        print("")
+
+
+register_all_game_data()
+print_items()
