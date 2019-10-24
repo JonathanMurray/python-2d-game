d1f7ffcd17ce067c6cb7571f0aafad9cd2591c4a
jmurray <jonte.murray@gmail.com>
2019-10-25 00:10:43 +0200

    Clean up old code from when chest was an enemy

--- pythongame/game_data/chests.py
+++ pythongame/game_data/chests.py
@@ -1,25 +1,13 @@
-from pythongame.core.common import Millis, Sprite, Direction
+from pythongame.core.common import Sprite, Direction
 from pythongame.core.game_data import SpriteSheet, register_entity_sprite_map
-from pythongame.core.game_state import GameState, NonPlayerCharacter, WorldEntity
 from pythongame.core.loot import LootTable, LootGroup
-from pythongame.core.npc_behaviors import AbstractNpcMind
-from pythongame.core.pathfinding.grid_astar_pathfinder import GlobalPathFinder
 from pythongame.game_data.loot_tables import LOOT_ITEMS_1, LOOT_ITEMS_2
 
 CHEST_ENTITY_SIZE = (30, 30)  # Must not align perfectly with grid cell size (pathfinding issues)
 CHEST_LOOT = LootTable([LootGroup(1, LOOT_ITEMS_1 + LOOT_ITEMS_2, 1)])
 
 
-class NpcMind(AbstractNpcMind):
-    def __init__(self, global_path_finder: GlobalPathFinder):
-        super().__init__(global_path_finder)
-
-    def control_npc(self, game_state: GameState, npc: NonPlayerCharacter, player_entity: WorldEntity,
-                    is_player_invisible: bool, time_passed: Millis):
-        pass
-
-
-def register_chest_enemy():
+def register_chest_entity():
     sprite = Sprite.CHEST
     sprite_sheet = SpriteSheet("resources/graphics/human_tileset.png")
     original_sprite_size = (32, 64)
