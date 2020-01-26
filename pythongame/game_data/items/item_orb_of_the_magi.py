3b7ce8df2b0f516b64d523115a5b27e87c674340
jmurray <jonte.murray@gmail.com>
2020-01-26 12:25:34 +0100

    Randomize spellcaster orbs

--- pythongame/game_data/items/item_orb_of_the_magi.py
+++ pythongame/game_data/items/item_orb_of_the_magi.py
@@ -1,25 +1,15 @@
 from pythongame.core.common import ItemType, Sprite, UiIconSprite, HeroStat
-from pythongame.core.game_data import register_ui_icon_sprite_path, register_item_data, ItemData, \
-    register_entity_sprite_initializer, SpriteInitializer, ITEM_ENTITY_SIZE
-from pythongame.core.item_effects import register_item_effect, StatModifyingItemEffect
 from pythongame.core.item_inventory import ItemEquipmentCategory
+from pythongame.game_data.items.register_items_util import register_randomized_stat_modifying_item
 
 
 def register_orb_of_the_magi_item():
-    item_types = [ItemType.ORB_OF_THE_MAGI_1, ItemType.ORB_OF_THE_MAGI_2, ItemType.ORB_OF_THE_MAGI_3]
-    multiplier_bonuses = [0.1, 0.15, 0.2]
-    ui_icon_sprite = UiIconSprite.ITEM_ORB_OF_THE_MAGI
-    sprite = Sprite.ITEM_ORB_OF_THE_MAGI
-    image_file_path = "resources/graphics/item_orb_of_the_magi.png"
-    register_ui_icon_sprite_path(ui_icon_sprite, image_file_path)
-    register_entity_sprite_initializer(
-        sprite, SpriteInitializer(image_file_path, ITEM_ENTITY_SIZE))
-    for i in range(3):
-        item_type = item_types[i]
-        bonus = multiplier_bonuses[i]
-        effect = StatModifyingItemEffect(item_type, {HeroStat.MAGIC_DAMAGE: bonus})
-        register_item_effect(item_type, effect)
-        name = "Orb of the Magi (" + str(i + 1) + ")"
-        description = effect.get_description()
-        item_data = ItemData(ui_icon_sprite, sprite, name, description, ItemEquipmentCategory.OFF_HAND)
-        register_item_data(item_type, item_data)
+    register_randomized_stat_modifying_item(
+        item_type=ItemType.ORB_OF_THE_MAGI,
+        ui_icon_sprite=UiIconSprite.ITEM_ORB_OF_THE_MAGI,
+        sprite=Sprite.ITEM_ORB_OF_THE_MAGI,
+        image_file_path="resources/graphics/item_orb_of_the_magi.png",
+        item_equipment_category=ItemEquipmentCategory.OFF_HAND,
+        name="Orb of the Magi",
+        stat_modifier_intervals={HeroStat.MAGIC_DAMAGE: [0.1, 0.12, 0.14, 0.16, 0.18, 0.2]}
+    )
--- pythongame/game_data/items/item_orb_of_wisdom.py
+++ pythongame/game_data/items/item_orb_of_wisdom.py
@@ -1,26 +1,15 @@
 from pythongame.core.common import ItemType, Sprite, UiIconSprite, HeroStat
-from pythongame.core.game_data import register_ui_icon_sprite_path, register_item_data, ItemData, \
-    register_entity_sprite_initializer, ITEM_ENTITY_SIZE
-from pythongame.core.item_effects import register_item_effect, StatModifyingItemEffect
 from pythongame.core.item_inventory import ItemEquipmentCategory
-from pythongame.core.view.image_loading import SpriteInitializer
+from pythongame.game_data.items.register_items_util import register_randomized_stat_modifying_item
 
 
 def register_orb_of_wisdom_item():
-    item_types = [ItemType.ORB_OF_WISDOM_1, ItemType.ORB_OF_WISDOM_2, ItemType.ORB_OF_WISDOM_3]
-    bonuses = [0.5, 0.75, 1]
-    ui_icon_sprite = UiIconSprite.ITEM_ORB_OF_WISDOM
-    sprite = Sprite.ITEM_ORB_OF_WISDOM
-    image_file_path = "resources/graphics/item_orb_of_wisdom.png"
-    register_ui_icon_sprite_path(ui_icon_sprite, image_file_path)
-    register_entity_sprite_initializer(
-        sprite, SpriteInitializer(image_file_path, ITEM_ENTITY_SIZE))
-    for i in range(3):
-        item_type = item_types[i]
-        bonus = bonuses[i]
-        effect = StatModifyingItemEffect(item_type, {HeroStat.MANA_REGEN: bonus})
-        register_item_effect(item_type, effect)
-        name = "Orb of Wisdom (" + str(i + 1) + ")"
-        description = effect.get_description()
-        item_data = ItemData(ui_icon_sprite, sprite, name, description, ItemEquipmentCategory.OFF_HAND)
-        register_item_data(item_type, item_data)
+    register_randomized_stat_modifying_item(
+        item_type=ItemType.ORB_OF_WISDOM,
+        ui_icon_sprite=UiIconSprite.ITEM_ORB_OF_WISDOM,
+        sprite=Sprite.ITEM_ORB_OF_WISDOM,
+        image_file_path="resources/graphics/item_orb_of_wisdom.png",
+        item_equipment_category=ItemEquipmentCategory.OFF_HAND,
+        name="Orb of Wisdom",
+        stat_modifier_intervals={HeroStat.MANA_REGEN: [0.5, 0.6, 0.7, 0.8, 0.9, 1]}
+    )
