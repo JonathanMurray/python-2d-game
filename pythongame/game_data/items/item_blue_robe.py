90c7c4f4256d9f6bb8ed04dffc34bcac6c1ecbad
jmurray <jonte.murray@gmail.com>
2020-01-26 12:17:07 +0100

    Randomize blue robe

--- pythongame/game_data/items/item_blue_robe.py
+++ pythongame/game_data/items/item_blue_robe.py
@@ -1,29 +1,15 @@
 from pythongame.core.common import ItemType, Sprite, UiIconSprite, HeroStat
-from pythongame.core.game_data import register_ui_icon_sprite_path, register_item_data, ItemData, \
-    register_entity_sprite_initializer, ITEM_ENTITY_SIZE
-from pythongame.core.item_effects import register_item_effect, StatModifyingItemEffect
 from pythongame.core.item_inventory import ItemEquipmentCategory
-from pythongame.core.view.image_loading import SpriteInitializer
+from pythongame.game_data.items.register_items_util import register_randomized_stat_modifying_item
 
 
 def register_blue_robe_item():
-    item_types = [ItemType.BLUE_ROBE_1, ItemType.BLUE_ROBE_2, ItemType.BLUE_ROBE_3]
-    mana_amounts = [10, 15, 20]
-    mana_regen_boost = 0.3
-    ui_icon_sprite = UiIconSprite.ITEM_BLUE_ROBE
-    sprite = Sprite.ITEM_BLUE_ROBE
-    register_ui_icon_sprite_path(ui_icon_sprite, "resources/graphics/item_blue_robe.png")
-    register_entity_sprite_initializer(
-        sprite, SpriteInitializer("resources/graphics/item_blue_robe.png", ITEM_ENTITY_SIZE))
-    for i in range(3):
-        item_type = item_types[i]
-        mana_amount = mana_amounts[i]
-        effect = StatModifyingItemEffect(item_type, {
-            HeroStat.MANA_REGEN: mana_regen_boost,
-            HeroStat.MAX_MANA: mana_amount
-        })
-        register_item_effect(item_type, effect)
-        name = "Blue Robe (" + str(i + 1) + ")"
-        description = effect.get_description()
-        item_data = ItemData(ui_icon_sprite, sprite, name, description, ItemEquipmentCategory.CHEST)
-        register_item_data(item_type, item_data)
+    register_randomized_stat_modifying_item(
+        item_type=ItemType.BLUE_ROBE,
+        ui_icon_sprite=UiIconSprite.ITEM_BLUE_ROBE,
+        sprite=Sprite.ITEM_BLUE_ROBE,
+        image_file_path="resources/graphics/item_blue_robe.png",
+        item_equipment_category=ItemEquipmentCategory.CHEST,
+        name="Blue Robe",
+        stat_modifier_intervals={HeroStat.MANA_REGEN: [0.3, 0.4], HeroStat.MAX_MANA: [10, 15, 20]}
+    )
