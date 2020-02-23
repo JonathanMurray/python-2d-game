f71258e8a67afd0d804166fd54d26d58c17ddfc6
jmurray <jonte.murray@gmail.com>
2020-02-23 13:55:41 +0100

    Continue unifying enemy registration

--- pythongame/game_data/enemies/enemy_goblin_warrior.py
+++ pythongame/game_data/enemies/enemy_goblin_warrior.py
@@ -1,8 +1,8 @@
 from pythongame.core.common import Millis, NpcType, Sprite, Direction, SoundId, LootTableId
-from pythongame.core.game_data import register_npc_data, NpcData, register_entity_sprite_map
-from pythongame.core.npc_behaviors import register_npc_behavior, MeleeEnemyNpcMind
+from pythongame.core.game_data import NpcData
+from pythongame.core.npc_behaviors import MeleeEnemyNpcMind
 from pythongame.core.pathfinding.grid_astar_pathfinder import GlobalPathFinder
-from pythongame.core.view.image_loading import SpriteSheet
+from pythongame.game_data.enemies.register_enemies_util import register_basic_enemy
 
 
 class NpcMind(MeleeEnemyNpcMind):
@@ -11,26 +11,30 @@ class NpcMind(MeleeEnemyNpcMind):
 
 
 def register_goblin_warrior_enemy():
-    size = (32, 32)  # Must not align perfectly with grid cell size (pathfinding issues)
-    sprite = Sprite.ENEMY_GOBLIN_WARRIOR
-    npc_type = NpcType.GOBLIN_WARRIOR
-    movement_speed = 0.09
-    health = 110
-    exp_reward = 50
-    npc_data = NpcData.enemy(sprite, size, health, 0, movement_speed, exp_reward, LootTableId.BOSS_GOBLIN,
-                             SoundId.DEATH_BOSS, is_boss=True)
-    register_npc_data(npc_type, npc_data)
-    register_npc_behavior(npc_type, NpcMind)
-    sprite_sheet = SpriteSheet("resources/graphics/enemy_sprite_sheet_2.png")
-    original_sprite_size = (32, 32)
-    scaled_sprite_size = (52, 52)
-    sprite_sheet_x = 3
-    sprite_sheet_y = 4
+    x = 3
+    y = 4
     indices_by_dir = {
-        Direction.DOWN: [(sprite_sheet_x + i, sprite_sheet_y + 0) for i in range(3)],
-        Direction.LEFT: [(sprite_sheet_x + i, sprite_sheet_y + 1) for i in range(3)],
-        Direction.RIGHT: [(sprite_sheet_x + i, sprite_sheet_y + 2) for i in range(3)],
-        Direction.UP: [(sprite_sheet_x + i, sprite_sheet_y + 3) for i in range(3)]
+        Direction.DOWN: [(x + i, y + 0) for i in range(3)],
+        Direction.LEFT: [(x + i, y + 1) for i in range(3)],
+        Direction.RIGHT: [(x + i, y + 2) for i in range(3)],
+        Direction.UP: [(x + i, y + 3) for i in range(3)]
     }
-    register_entity_sprite_map(sprite, sprite_sheet, original_sprite_size, scaled_sprite_size, indices_by_dir,
-                               (-10, -20))
+
+    register_basic_enemy(
+        npc_type=NpcType.GOBLIN_WARRIOR,
+        npc_data=NpcData.enemy(
+            sprite=Sprite.ENEMY_GOBLIN_WARRIOR,
+            size=(32, 32),
+            max_health=110,
+            health_regen=0,
+            speed=0.09,
+            exp_reward=50,
+            enemy_loot_table=LootTableId.BOSS_GOBLIN,
+            death_sound_id=SoundId.DEATH_BOSS),
+        mind_constructor=NpcMind,
+        spritesheet_path="resources/graphics/enemy_sprite_sheet_2.png",
+        original_sprite_size=(32, 32),
+        scaled_sprite_size=(52, 52),
+        spritesheet_indices=indices_by_dir,
+        sprite_position_relative_to_entity=(-10, -20)
+    )
